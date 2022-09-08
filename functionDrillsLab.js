ec86d7edcafaf9300171ea5626ff90dc9daba3fa
Darren Wott <darren.wott@gmail.com>
2022-09-08 01:52:28 -0700

    finished more problems

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -325,6 +325,16 @@ makeUpperCase(sampleString)
   return 'must provide a valid email address'
 */
 
+//CODE HERE
+function emailCheck(email) {
+  email = String(email).trim()
+  if (email.includes('@')) {
+    return 'email verified'
+  } else {
+    return 'must provide valid email address'
+  }
+}
+
 ////////////////// PROBLEM 19 ////////////////////
 /*
   Write a function, naming it whatever you believe to be appropriate, that buys as many chocolate frogs as possible with a certain amount of gold. Each chocolate frog costs 3 gold. Your function should take in a single parameter, which is the amount of gold you are willing to spend. Your function should return a total amount of chocolate frogs you were able to purchase.
@@ -332,7 +342,11 @@ makeUpperCase(sampleString)
 */
 
 //CODE HERE
+function wanka(gold) {
+  return gold / 3
+}
 
+let totalFrogs = wanka(50)
 
 ////////////////// PROBLEM 20 ////////////////////
 /*
